33d7d84f9c16ea5347583872571ef203f495b813
wizzlerr <sabel.adam@gmail.com>
2017-07-15 09:59:47 +0200

    Initial commit

---
+++ src/main/java/com/ubs/opsit/interviews/BerlinClock.java
@@ -0,0 +1,13 @@
+package com.ubs.opsit.interviews;
+
+/**
+ * Created by Adam on 2017-07-15.
+ */
+public class BerlinClock implements TimeConverter {
+
+    @Override
+    public String convertTime(String aTime) {
+        return null;
+    }
+    
+}
